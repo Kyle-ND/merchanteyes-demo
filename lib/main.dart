50e358946f3a44113a1ab38bcd93617231e29439
Kyle <kndlovu023@student.wethinkcode.co.za>
2024-02-13 15:17:20 +0200

    added new homepage look

--- lib/main.dart
+++ lib/main.dart
@@ -23,47 +23,77 @@ class MyHome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: logotitle(),
-      centerTitle: false,
-      backgroundColor: Color.fromARGB(255, 8, 198, 231),
-      actions: [ IconButton(onPressed: () => {}, icon: Icon(Icons.person)),],),
-      bottomNavigationBar: BottomNavigationBar(
-      backgroundColor: Colors.cyanAccent,
-      unselectedItemColor: Colors.cyan,
-      selectedItemColor: Colors.black,
-      items: const [
-        BottomNavigationBarItem(
-        icon: Icon(Icons.home),
-        label: "Home",),
-        BottomNavigationBarItem(
-        icon: Icon(Icons.search),
-        label: "Search",),
-        BottomNavigationBarItem(
-        icon: Icon(Icons.task),
-        label: "Stock",),
-        BottomNavigationBarItem(
-        icon: Icon(Icons.analytics),
-        label: "Insights",)
-      ])
-    );
+        appBar: AppBar(
+          title: _logo(),
+          centerTitle: false,
+          backgroundColor: Color.fromARGB(255, 8, 198, 231),
+          actions: [
+            IconButton(onPressed: () => {}, icon: ImageIcon(
+              AssetImage('assets/images/profileicon.png'),size: 48,)),
+          ],
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+            backgroundColor: Colors.cyanAccent,
+            unselectedItemColor: Colors.cyan,
+            selectedItemColor: Colors.black,
+            items: const [
+              BottomNavigationBarItem(
+                icon: ImageIcon(AssetImage('assets/images/homebuttonicon.png'),
+                size: 48.0,
+                color: Colors.cyan,),
+                label: "Home"
+              )
+              ,
+              BottomNavigationBarItem(
+                icon: ImageIcon(
+                  AssetImage('assets/images/MerchantEyes icons.png'),
+                  size: 48.0,
+                  color: Colors.cyan,
+                ),
+                label: "Trade",
+              ),
+              BottomNavigationBarItem(
+                icon: ImageIcon(
+                  AssetImage('assets/images/stock.png'),
+                  size: 48.0,
+                  color: Colors.cyan,
+                ),
+                label: "Stock",
+              ),
+              BottomNavigationBarItem(
+                icon: ImageIcon(
+                  AssetImage('assets/images/insights.png'),
+                  size: 48.0,
+                  color: Colors.cyan,
+                ),
+                label: "Insights",
+              )
+            ]));
+  }
+
+  Widget _logo() {
+    return Image.asset('assets/images/MerchantEyes icons-02.png',fit: BoxFit.cover,height: 155,);
   }
 }
 
 // ignore: camel_case_types
-class logotitle extends StatelessWidget{
-
+class logotitle extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
-      children: [Text("Merchant Eyes",
-       style: TextStyle(
-        fontSize: 30,
-        foreground: Paint()
-          ..style = PaintingStyle.stroke
-          ..strokeWidth = 2
-          ..color = Color.fromARGB(255, 0, 0, 0)!,
-      ),),],
+      children: [
+        Text(
+          "Merchant Eyes",
+          style: TextStyle(
+            fontSize: 30,
+            foreground: Paint()
+              ..style = PaintingStyle.stroke
+              ..strokeWidth = 2
+              ..color = Color.fromARGB(255, 0, 0, 0),
+          ),
+        ),
+      ],
     );
   }
 }
